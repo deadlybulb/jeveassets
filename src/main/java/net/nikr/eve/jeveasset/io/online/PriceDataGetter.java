8ee71b66696b0aa40022e6f75da8ba12f5db1dfa
GoldenGnu <nkr@niklaskr.dk>
2010-07-17 13:43:23 +0000

    Branching the pricing changes

--- src/main/java/net/nikr/eve/jeveasset/io/online/PriceDataGetter.java
+++ src/main/java/net/nikr/eve/jeveasset/io/online/PriceDataGetter.java
@@ -28,6 +28,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Proxy;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -51,11 +52,12 @@ public class PriceDataGetter implements PricingListener {
 
 	private Settings settings;
 	private UpdateTask updateTask;
-	private boolean updated;
 	private long nextUpdate = 0;
 	private long priceCacheTimer = 60*60*1000l; // 1 hour
 	private boolean enableCacheTimers = true;
 	private Map<Integer, PriceData> priceDataList;
+	private List<Integer> failedIds;
+	private int retries = 1;
 
 	public PriceDataGetter(Settings settings) {
 		this.settings = settings;
@@ -64,7 +66,6 @@ public class PriceDataGetter implements PricingListener {
 	public boolean load(UpdateTask task, boolean forceUpdate, boolean enableCacheTimers){
 		this.updateTask = task;
 		this.enableCacheTimers = enableCacheTimers;
-		updated = false;
 
 		if (forceUpdate){
 			LOG.info("Price data updating:");
@@ -75,6 +76,7 @@ public class PriceDataGetter implements PricingListener {
 		}
 		//Create new price data map (Will only be used if task complete)
 		priceDataList = new HashMap<Integer, PriceData>();
+		failedIds = new ArrayList<Integer>();
 
 		//Get all price ids
 		List<Integer> ids = settings.getUniqueIds();
@@ -82,6 +84,7 @@ public class PriceDataGetter implements PricingListener {
 		PricingFactory.setPricingOptions( new EveAssetPricingOptions() );
 		Pricing pricing = PricingFactory.getPricing();
 		pricing.addPricingListener(this);
+		pricing.resetAllAttemptCounters();
 		//Reset cache timers...
 		if (forceUpdate){
 			for (int a = 0; a < ids.size(); a++){
@@ -94,26 +97,43 @@ public class PriceDataGetter implements PricingListener {
 			createPriceData(ids.get(a), pricing);
 		}
 		//Wait to complete
-		while (settings.getUniqueIds().size() >  priceDataList.size()){
+		while (ids.size() >  (priceDataList.size() + failedIds.size())){
 			try {
 				synchronized(this) {
                     wait();
                 }
 			} catch (InterruptedException ex) {
 				LOG.info("Failed to update price");
-				this.updateTask.addError("Price data", "Cancelled");
-				this.updateTask.setTaskProgress(100, 100, 0, 100);
-				this.updateTask = null;
+				pricing.cancelAll();
+				if (updateTask != null){
+					updateTask.addError("Price data", "Cancelled");
+					updateTask.setTaskProgress(100, 100, 0, 100);
+					updateTask = null;
+				}
+
 				return false;
 			}
 		}
-		settings.setPriceData( priceDataList );
-		if (!enableCacheTimers && updated){
-			LOG.info("	Price data loaded (updated as needed)");
-		} else if (!enableCacheTimers) {
-			LOG.info("	Price data loaded");
-		} else if (updated){
-			LOG.info("	Price data updated");
+		boolean updated = !priceDataList.isEmpty();
+		boolean failed = !failedIds.isEmpty();
+
+		if (updated){
+			if (!failed){
+				if (!enableCacheTimers && updated){
+				LOG.info("	Price data loaded (updated as needed)");
+				} else if (!enableCacheTimers) {
+					LOG.info("	Price data loaded");
+				} else if (updated){
+					LOG.info("	Price data updated");
+				}
+				settings.setPriceData( priceDataList );
+			} else {
+				LOG.info("	Some of the price data was updated");
+				if (updateTask != null) this.updateTask.addError("Price data", "Only some of the price data was updated");
+			}
+		} else {
+			LOG.info("	Failed to update price data");
+			if (updateTask != null) this.updateTask.addError("Price data", "Failed to update price data");
 		}
 		try {
 			pricing.writeCache();
@@ -131,7 +151,14 @@ public class PriceDataGetter implements PricingListener {
 	@Override
 	public void priceUpdated(int typeID, Pricing pricing) {
 		createPriceData(typeID, pricing);
-		updated = true;
+		synchronized(this) {
+			notify();
+		}
+	}
+
+	@Override
+	public void priceUpdateFailed(int typeID, Pricing pricing) {
+		failedIds.add(typeID);
 		synchronized(this) {
 			notify();
 		}
@@ -172,7 +199,7 @@ public class PriceDataGetter implements PricingListener {
 
 		@Override
 		public long getPriceCacheTimer() {
-			return priceCacheTimer; // 1 hour
+			return priceCacheTimer;
 		}
 
 		@Override
@@ -217,6 +244,11 @@ public class PriceDataGetter implements PricingListener {
 		@Override
 		public Proxy getProxy() {
 			return settings.getProxy();
+		}
+
+		@Override
+		public int getAttemptCount() {
+			return retries;
 		}
 	}
 }
